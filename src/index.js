587f129d7c0a2f7b6d0ac122df2768715ad822fe
Minskoleopc <minskoleopc@gmail.com>
2023-03-15 19:30:04 +0530

    added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
+// 1) Import the React and ReactDOM libraries
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Get the reference to the div with the root
+
+const el = document.getElementById('root')
+// Tell react to the control of that element 
+const root = ReactDOM.createRoot(el)
+
+// Creating a component
+function App(){
+  // rule 
+  // element should be returned from a compoment to view
+  //<h3>Hello</h3>
+  //return <h1>Hi there!</h1>
+
+
+  // program2 (Numbers and string )
+  // let message = "chinmay"
+  // let age = 32
+  // return <h1>{message} {age}</h1>
+
+  // program 3 boolean
+
+  //let canDrive = true
+  //let canDrive = {firstName:"chinmay" ,lastName:"deshpande"}
+  //let canDrive = ["sarika","poorva","mayuri"]
+  //let canDrive = [1,2,3]
+  // let date  = new Date()
+  // let time =  date.toLocaleTimeString()
+  // return <h1>{time}</h1>
+
+  // program 4
+  let inputType = "number"
+  let mina = 5
+  let maxa = 10
+  return <input  className = "hello" type={inputType} min= {mina} max={maxa}/>
+
+
+
+
+
+}
+// show the component on UI 
+
+root.render(<App/>)
